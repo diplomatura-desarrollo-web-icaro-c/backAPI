2410725a7ff1743d6ddede9f169e7cd61e8ef1bc
Norberto <norberto.cabrera.a@gmail.com>
2022-04-18 21:10:16 -0300

    app que consume la api backend y muestra el objeto de usuarios

---
+++ FrontEndEjemplo.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3001;
+const fetch = require("node-fetch");
+
+app.listen(port, () => {
+  console.log(`listening front end on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("front funcionando ok");
+});
+
+app.get("/usuarios", (req, res) => {
+  let respuesta = fetch("http://localhost:3000/all")
+    .then((response) => response.json())
+    .then((data) => res.send(data));
+});
